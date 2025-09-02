e069cb9fa50379944053ef41dc5571ae35343312
genesisvaca <138408577+genesisvaca@users.noreply.github.com>
2025-09-02 20:17:58 +0200

    feat: Mesozoic Eden Exercise - Getting Started with Java

---
+++ Getting_Started_with_Java/Exercises/MesozoicEden/src/main/java/MesozoicEden.java
@@ -0,0 +1,25 @@
+public class MesozoicEden {
+    public static void main(String[] args){
+
+        String welcomeName = "Izan";
+        System.out.println( "\n\t\t\t\t HEY " + welcomeName + ", WELCOME to Mesozoic Eden");
+
+        System.out.println("\n\t\t\t\t************************************");
+        System.out.println("\t\t\t\t||Mesozoic Eden is safe and secure||");
+        System.out.println("\t\t\t\t************************************\n");
+
+        String name ="Abi";
+        String position = "Park Manager";
+
+        System.out.println("\tMy name is "+ name + ", and I am the " + position + " of Mesozoic Eden.");
+        System.out.println("\tHere some helpful info:");
+
+        String openingHours = "08:00";
+        String closingHours = "20:00";
+
+        System.out.println("\n\t\t\t\tOPENING HOURS:\t\t\t\t" + openingHours);
+        System.out.println("\n\t\t\t\tCLOSING HOURS:\t\t\t\t" + closingHours);
+
+        System.out.println("\n\t\t\t We really hope you'll enjoy your time with us!");
+    }
+}
