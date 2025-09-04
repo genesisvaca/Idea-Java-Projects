41abfeac62c182a38ff89baa42ab72d758288b08
genesisvaca <138408577+genesisvaca@users.noreply.github.com>
2025-09-04 19:18:07 +0200

    feat: Variables and Primitive Data Types

---
+++ Variables_and_Primitive_Data_Type/MesoczoicEden/src/main/java/MesozoicEden.java
@@ -0,0 +1,58 @@
+public class MesozoicEden {
+    public static void main (String[] args){
+
+        String breed = "Tyrannosaurus Rex";
+        String height = "4 m";      // hip height
+        String length = "12 m";     // head to tail
+        String weight = "8,000 kg";
+        String dinoName = "Erick";
+        Integer dinoAge = 7;
+        Integer totalDinos = 47;
+        String carnivore = "carnivore";
+        String herbivore = "herbivore";
+        Character tyrannosaurusRex = 't';
+
+        // Employee profile
+        String employeeName = "Izan";
+        Integer employeeAge = 24;
+
+        Integer parkSafety = 10;
+
+        System.out.println("\n\t\tMeet " + dinoName + ",\n\t\ta " + dinoAge + "-year-old "+ breed + ".\n\t\tAs a " + carnivore + ", it has a robust weight\n\t\tof " + weight + " kilograms.");
+
+        System.out.println("\n\t/////////////////////////////////////////////");
+        System.out.println("\t|\t\t\t" + breed + "\t\t\t(" + tyrannosaurusRex + ")\t|");
+        System.out.println("\t/////////////////////////////////////////////");
+        System.out.println("\t|\t\tNAME: \t\t\t\t" + dinoName + "\t\t\t|");
+        System.out.println("\t|\t\tAGE: \t\t\t\t" + dinoAge + "\t\t\t\t|");
+        System.out.println("\t|\t\tHEIGHT: \t\t\t" + height + "\t\t\t\t|");
+        System.out.println("\t|\t\tLENGTH: \t\t\t" + length + "\t\t\t|");
+        System.out.println("\t|\t\tWEIGHT: \t\t\t" + weight + "\t\t|");
+        System.out.println("\t|\t\tDIET: \t\t\t\t" + carnivore + "\t\t|");
+        System.out.println("\t/////////////////////////////////////////////");
+
+        System.out.println("\n\t/////////////////////////////////////////////");
+        System.out.println("\t|\t\t\t\tEmployee list\t\t\t\t|");
+        System.out.println("\t/////////////////////////////////////////////");
+        System.out.println("\t|\t\tNAME: \t\t\t\t" + employeeName + "\t\t\t|");
+        System.out.println("\t|\t\tAGE: \t\t\t\t" + employeeAge + "\t\t\t\t|");
+        System.out.println("\t/////////////////////////////////////////////");
+
+        System.out.println("\n\t/////////////////////////////////////////////");
+        System.out.println("\t|\t\tTotal Dinos in Mesozoic Park\t\t|");
+        System.out.println("\t/////////////////////////////////////////////");
+        System.out.println("\t|\t\t\t\t\t\t\t\t\t\t" + totalDinos+ "\t|");
+        System.out.println("\t/////////////////////////////////////////////");
+
+        System.out.println("\n\t/////////////////////////////////////////////");
+        System.out.println("\t|\t\t\tSafety in Mesozoic Park\t\t\t|");
+        System.out.println("\t/////////////////////////////////////////////");
+        System.out.println("\t|\t\t\t\t\t\t\t\t\t" + parkSafety+ "/10\t|");
+        System.out.println("\t/////////////////////////////////////////////");
+
+
+
+
+
+    }
+}
